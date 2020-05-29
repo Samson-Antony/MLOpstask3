329b509d300106b07019bcb03d6dd1348f620dd8
Antony Samson P J <49990770+Samson-Antony@users.noreply.github.com>
2020-05-30 02:36:14 +0530

    Update tweak.py

--- tweak.py
+++ tweak.py
@@ -24,7 +24,7 @@ if acc3<95:
        exit()
    os.system("sed -i '/softmax/ i {}' /root/tensor/mnist.py".format(y))
    os.system("curl -u admin:root http://192.168.1.103:8080/job/tjob3/build?token=root")
-   acc = os.popen("cat /root/tensor/accuracy.txt")
+   acc = os.popen("cat /root/tensor/file.txt")
    acc1 = acc.read()
    print(acc1)
    acc2 = acc1.rstrip()
