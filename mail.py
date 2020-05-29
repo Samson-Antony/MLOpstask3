780fa5e1c958f93ccbd0e97a15d57e1fe80145d1
Antony Samson P J <49990770+Samson-Antony@users.noreply.github.com>
2020-05-30 03:36:16 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -10,7 +10,7 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 host_address = "samsonjac53@gmail.com"
-host_pass = "samsonpalam"
+host_pass = "****"
 guest_address = "samsonjac53@gmail.com"
 subject = "Regarding Success of your model "
 content = "congratulations your model trained successfully with"+ str(a)+ "accuracy";
