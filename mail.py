7c1c85b3c151925e17eb3eba51a7d5b92f82b81b
Antony Samson P J <49990770+Samson-Antony@users.noreply.github.com>
2020-05-30 02:13:25 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,17 +1,17 @@
-#!/usr/bin/env python
+r#!/usr/bin/env python
 # coding: utf-8
 
 # In[4]:
 
 
-f= open("/root/code/file.txt" ,"r")
+f= open("/root/tensor/file.txt" ,"r")
 a=f.read()
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-host_address = "*****@gmail.com"
-host_pass = "*****"
-guest_address = "******@gmail.com"
+host_address = "samsonjac53@gmail.com"
+host_pass = "samsonpalam"
+guest_address = "samsonjac53@gmail.com"
 subject = "Regarding Success of your model "
 content = "congratulations your model trained successfully with"+ str(a)+ "accuracy";
 message = MIMEMultipart()
